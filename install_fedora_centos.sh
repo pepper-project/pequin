bc7c39a1981c56d53b56898af589da47c9810aea
Max Howald <maxhowald@gmail.com>
2016-10-02 05:10:53 -0400

    add libssl-dev to dependency installation scripts

--- install_fedora_centos.sh
+++ install_fedora_centos.sh
@@ -3,7 +3,7 @@ set -e
 sudo yum groupinstall 'Development Tools'
 sudo yum install python python-devel java-1.8.0-openjdk-devel golang gcc   \
         gcc-c++ glibc-devel libstdc++ glibc-devel.i686 libstdc++.i686 ant \
-        make time boost boost-devel gmp gmp-devel zlib zlib-devel
+        make time boost boost-devel gmp gmp-devel zlib zlib-devel openssl-devel
 
 cd thirdparty
 ./install_pepper_deps.sh
