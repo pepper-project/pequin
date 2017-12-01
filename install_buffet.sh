a82a55f97d9fecc01e5807f934912fb3707da2dd
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-12-01 13:55:56 +0100

    Update llvm to the latest version
    - Updated patches
    - Create a tar with the latest version of llvm patched

--- install_buffet.sh
+++ install_buffet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 cd compiler/buffetfsm/
-tar xjvf ../../thirdparty/llvm_clang-r209914-patched-buffet_fsm_patch.tar.bz2
+tar xjvf ../../thirdparty/llvm_clang-r319532-patched-buffet_fsm_patch.tar.bz2
 mkdir toolchain
 mkdir llvm-build
 cd llvm-build
-$PWD/../llvm/configure --prefix=$PWD/../toolchain --enable-shared --enable-optimized
+cmake -DCMAKE_INSTALL_PREFIX=$PWD/../toolchain -DBUILD_SHARED_LIBS=True -DLLVM_OPTIMIZED_TABLEGEN=True $PWD/../llvm/
 make -j$(nproc)
