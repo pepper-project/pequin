bf546c9d9de3e5f2c5b7d2e139b9459ad88a1c38
Max Howald <maxhowald@gmail.com>
2018-09-07 10:33:05 -0400

    include cmake as dependency in installation scripts for building libsnark

--- install_arch.sh
+++ install_arch.sh
@@ -3,7 +3,7 @@ set -e
 sudo pacman -S python python2 jdk7-openjdk go base-devel multilib-devel    \
             libstdc++5 lib32-glibc lib32-libstdc++5 lib32-gcc-libs         \
             gcc-libs-multilib libtool-multilib gcc-multilib apache-ant     \
-            make time boost boost-libs gmp zlib
+            make time boost boost-libs gmp zlib cmake
 
 
 cd thirdparty
