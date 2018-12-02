d83ec12a41525ec03aa1b0e8579ef1c205bed8ed
Dimitris Mouris <jimouris@gmail.com>
2018-12-02 15:02:25 +0200

    Fix some minor typos
    Fix some typos

--- pepper/apps/prover_knows_factorization.h
+++ pepper/apps/prover_knows_factorization.h
@@ -5,14 +5,14 @@
 /**
  * Explanation and usage of NUM_OF_FACTORS_UPPER_BOUND
  *
- * Large primes have more than just 2 factors. The number of factors should be oblivious 
+ * Large numbers have more than just 2 factors. The number of factors should be oblivious 
  * to both the program (pepper/apps/prover_knows_factorization.c) and the verifier.
  * Thus, we use an upper bound (NUM_OF_FACTORS_UPPER_BOUND = 10) to the number of factors.
  * If the factors are less than NUM_OF_FACTORS_UPPER_BOUND, the rest should be filled with 1.
 **/
 
 struct In {
-    uint32_t prime[1];            			/* public prime */
+    uint32_t input_num[1];            			/* public input number */
 };
 
 struct Out {
